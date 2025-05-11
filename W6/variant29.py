3533152c9593627c3825da766a7e08d6b64b5ebb
notfani <notfaniii@gmail.com>
2025-05-11 22:18:43 +0300

    homework 6 adding

--- W6/variant29.py
+++ W6/variant29.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def compute_set_I(input_set):
     """Формирует множество set_I."""
     return {abs(k) + 4 * k for k in input_set if k <= 65}
@@ -44,38 +47,24 @@ def compute_pairs(set_I, set_Z):
 def compute_nu(input_set):
     # Формирование множеств
     set_I = compute_set_I(input_set)
-    print(f"Set I: {set_I}")
-
     set_Delta = compute_set_Delta(input_set)
-    print(f"Set Delta: {set_Delta}")
-
     set_Z = compute_set_Z(set_I, set_Delta)
-    print(f"Set Z: {set_Z}")
 
     # Вычисление сумм
     sum_I = compute_sum_I(set_I)
-
     pairs = compute_pairs(set_I, set_Z)
-    for i, z, prod in pairs[:10]:  # Выводим только первые 10 пар
-        print(f"Pair: ({i}, {z}) -> {prod}")
-
     sum_Z = compute_sum_Z(pairs)
 
     # Вычисление ν
     return sum_I - sum_Z
 
 
-def main():
-    try:
-        # Чтение данных с клавиатуры
-        input_data = input("Введите множество чисел через пробел: ")
-        input_set = set(map(int, input_data.split()))
-        result = compute_nu(input_set)
-        print("Результат вычислений:", result)
-    except ValueError:
-        print("Ошибка: введите только целые числа через пробел.")
+def main(input_set):
+    result = compute_nu(input_set)
+    return result
 
 
 # Точка входа для выполнения
 if __name__ == "__main__":
-    main()
+    input_set = set(map(int, sys.argv[1:]))
+    print(main(input_set))
